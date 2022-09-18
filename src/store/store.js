0948acb1769bbdda51d3af48a5ad1057041705c7
tatekitzhak <tatekosi@gmail.com>
2022-09-18 12:51:09 +0300

    redux-persist - how do Blacklist & Whitelist

--- src/store/store.js
+++ src/store/store.js
@@ -24,6 +24,35 @@ const persistConfig = {
   key: 'root',
   storage,
 }
+/*
+
+** https://github.com/rt2zz/redux-persist
+** https://blog.reactnativecoach.com/the-definitive-guide-to-redux-persist-84738167975
+
+Blacklist & Whitelist
+By Example:
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  whitelist: ["state_which_you_want_to_persist"],       // Will be persisted
+  blacklist: ["state_which_you_do_not_want_to_persist"] // Will not be persisted
+};
+
+combineReducers({ 
+  auth: AuthReducer,
+  navigation: NavReducer, 
+  notes: NotesReducer
+});
+
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  blacklist: ["navigation"], // Will not be persisted
+  whitelist: ["notes", auth],      // Will be persisted
+};
+
+*/
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
